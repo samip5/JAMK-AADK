6c05114471eb3665dcb327aed1f5b8c25d2c2cc2
Sami Mäntysaari <sm@samip.fi>
2019-05-16 06:53:54 +0300

    Started making my first Android Application. WIP.

---
+++ FirstApplication/app/src/main/java/dev/samip/firstapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package dev.samip.firstapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun buttonClicked(view: View) {
+        // activity_main layout has textView id in TextView element
+        textView.setText(R.string.button_clicked_txt);
+    }
+}
