9a63d574f0c52d02f99354e5291f6c5ee1640cb0
Sami Mäntysaari <sm@samip.fi>
2019-07-07 02:01:20 +0300

    BuildYourFirstApp with multiple intents, done.
    However for some reason it does not want to work correctly even though
    it's like the one at Google Developers site. @mapas

---
+++ BuildYourFirstApp/app/src/main/java/dev/samip/buildyourfirstapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package dev.samip.buildyourfirstapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "dev.samip.BuildYourFirstApp.MESSAGE"
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.textView)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+
+}
