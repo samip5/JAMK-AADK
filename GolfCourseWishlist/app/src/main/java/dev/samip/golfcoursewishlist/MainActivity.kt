3c6914ea87d7fc2c47ba7b0c3a39a3db81ff87cc
Skyler M <skyler.mantysaari@edu.turkuamk.fi>
2019-11-09 22:39:12 +0200

    Added Ads to the GolfCourse app.
    There's no screenshot of it as I cannot get it to work in my network, due to blocking all ads on DNS level.

--- GolfCourseWishlist/app/src/main/java/dev/samip/golfcoursewishlist/MainActivity.kt
+++ GolfCourseWishlist/app/src/main/java/dev/samip/golfcoursewishlist/MainActivity.kt
@@ -3,13 +3,31 @@ package dev.samip.golfcoursewishlist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.row_places.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mAdView : AdView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // Initialize Mobile Ads SDK
+        MobileAds.initialize(this) {}
+
+        mAdView = findViewById(R.id.ad_view)
+
+        // Create an ad request.
+        val adRequest = AdRequest.Builder().build()
+
+        // Start loading the ad in the background
+        mAdView.loadAd(adRequest)
 
         // Use StaggeredGridLayoutManager as a layout manager for recyclerView
         recyclerView.layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
