fd4f864206163d967bdf382231924af394442d24
Skyler M <skyler.mantysaari@edu.turkuamk.fi>
2019-11-16 15:19:47 +0200

    ShowPlacesInMap: Project done and screenshots added.

--- ShowPlacesInMap/app/src/main/java/dev/samip/showplacesinmap/MapsActivity.kt
+++ ShowPlacesInMap/app/src/main/java/dev/samip/showplacesinmap/MapsActivity.kt
@@ -2,7 +2,9 @@ package dev.samip.showplacesinmap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
+import com.google.android.gms.maps.CameraUpdateFactory
 
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -23,12 +25,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    fun fetchDataFromJson() {
+
+    private fun fetchDataFromJson() {
         val remoteEndpoint = "https://samip.dev/android/map_data.json"
 
         val request = Request.Builder().url(remoteEndpoint).build()
@@ -40,22 +42,28 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             }
 
             override fun onResponse(call: Call, response: Response) {
-                val body = response.body()?.string()
-                Log.i("JSON", body.toString())
+                runOnUiThread {
+                    val body = response.body()?.string()
+                    Log.i("JSON", body.toString())
 
-                val gson = GsonBuilder().create()
+                    val gson = GsonBuilder().create()
 
-                mfromJson = gson.fromJson(body, ResponseModel::class.java)
-                if(mfromJson.data != null) {
-                    Log.d("onResponse_JSON", "There is data!")
-                }
-                for(item in mfromJson.data!!) {
-                    val city = item!!.marker_name
-                    val latutude_long = LatLng(item!!.lat!!, item.lo!!)
-                    mMap.addMarker(MarkerOptions().position(latutude_long).title(city))
+
+                    mfromJson = gson.fromJson(body, ResponseModel::class.java)
+                    if(mfromJson.data != null) {
+                        Log.d("onResponse_JSON", "There is data!")
+                    }
+                    for(item in mfromJson.data!!) {
+                        val city = item!!.marker_name
+                        val latutude_long = LatLng(item!!.lat!!, item.lo!!)
+                        mMap.addMarker(MarkerOptions().position(latutude_long).title(city))
+                    }
+                    mMap.uiSettings.isZoomControlsEnabled = true
+                    val kupittaa = LatLng(60.449446798243365, 22.291838048566213)
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kupittaa, 14.0F))
+
                 }
             }
-
         })
     }
 
