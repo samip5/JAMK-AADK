4fbba166121962dc7ff424d1af6693898f41d558
Skyler M <skyler.mantysaari@edu.turkuamk.fi>
2019-11-18 22:06:45 +0200

    RoomWordSample: Project Done.

---
+++ RoomWordSample/app/src/main/java/dev/samip/jamk/roomwordsample/db/WordRepository.kt
@@ -0,0 +1,13 @@
+package dev.samip.jamk.roomwordsample.db
+
+import androidx.lifecycle.LiveData
+
+class WordRepository(private val wordDao: WordDao) {
+    // Room executes all queries on a separate thread.
+    // Observed LiveData will notify the observer when the data has changed.
+    val allWords: LiveData<List<Word>> = wordDao.getAlphabetizedWords()
+
+    suspend fun insert(word: Word) {
+        wordDao.insert(word)
+    }
+}
